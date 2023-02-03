d0ac64e4d735ba571bc16897cefdf4ff4a080545
shyam tailor <shyamtailorst123@gmail.com>
2023-02-04 00:17:52 +0530

    db password

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ module.exports = {
         var con = mysql.createConnection({
             host: "bn99lxpd9gmcm6atvdxz-mysql.services.clever-cloud.com",
             user: "u8vqt51zfn4lovkj",
-            password: 'u8vqt51zfn4lovkj',
+            password: 'lyjR5vAW9w7OK6O5rOYA',
             database: 'bn99lxpd9gmcm6atvdxz'
         })
         con.connect((err, result) => {
